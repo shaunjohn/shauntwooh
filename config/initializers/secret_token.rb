28c4f9f8cdbedceb94b272d14309b704d8993d42
Shaun Johnson <shaunejohnson@Shaun-E-Johnsons-MacBook-Pro.local>
2012-12-19 20:52:48 -0500

    My 1st push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shauntwooh::Application.config.secret_token = '0682cbbeeba21d51f5c5928a3e27a4e657e0dc85a8244160dd8d0bf93265b2187c8dd3a1833b277c4ba57c6cee50c9026acd226316975942e368cfe1b9c55718'
